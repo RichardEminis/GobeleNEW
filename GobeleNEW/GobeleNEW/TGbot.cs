bc4ed1da3b9159dbba23c76aebb9ed7d2106d20a
Alexandr Belyaev <eminis.ru@gmail.com>
2022-12-27 01:04:14 +0300

    Edit TGbot

--- GobeleNEW/GobeleNEW/TGbot.cs
+++ GobeleNEW/GobeleNEW/TGbot.cs
@@ -1,12 +1,59 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
 
 namespace GobeleNEW
 {
-    internal class TGbot
+    public class TGbot
     {
+        ITelegramBotClient _bot;
+
+        public TGbot ()
+        {
+            string token = @"5949583454:AAFlr-rcBB0BxcKCfZflphAEBHT2w8Ej33w";
+            _bot = new TelegramBotClient(token);
+
+            Console.WriteLine("Запущен бот " + _bot.GetMeAsync().Result.FirstName);
+            var cts = new CancellationTokenSource();
+            var cancellationToken = cts.Token;
+            var receiverOptions = new ReceiverOptions
+            {
+                AllowedUpdates = { },
+            };
+
+            _bot.StartReceiving(
+                HandleUpdateAsync,
+                HandleErrorAsync,
+                receiverOptions,
+                cancellationToken
+            );
+
+            public async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+            {
+                Console.WriteLine(update.Message.Chat.FirstName + " " + update.Message.Chat.LastName);
+                if (update.Message.Text is not null)
+                {
+                    Console.WriteLine(update.Message.Text);
+                    if (update.Message.Text.ToLower() == "/start")
+                    {
+                        _bot.SendTextMessageAsync(update.Message.Chat.Id, $"Обзови меня!");
+                    }
+                    else
+                    {
+                        _bot.SendTextMessageAsync(update.Message.Chat.Id, $"Сам ты - {update.Message.Text}");
+                    }
+                }
+            }
+
+            public async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+            {
+
+            }
+        }
+        
     }
 }
